d3204bc7b3296c0c776406777a81841f663e5add
cdfritz7 <cdfritz7@utexas.edu>
2019-09-16 15:26:25 -0500

    added average to edge detection

--- exploration/real_time_video/real_time_edge.cpp
+++ exploration/real_time_video/real_time_edge.cpp
@@ -9,8 +9,13 @@ int main() {
     std::exit(-1);
   }
 
+  std::vector<cv::Mat> prev_images;
   cv::Mat image;
-  double FPS = 60.0;
+  cv::Mat cannyResult;
+  cv::Mat avg_result;
+  int prev_count = 10; //length of averaging indow
+  double FPS = 30.0;
+
   //read and display camera frames until q is pressed
   while(true){
     cap >> image;
@@ -20,11 +25,28 @@ int main() {
       break;
     }
 
-    cv::Mat cannyResult;
-    cv::Canny(image, cannyResult, 50, 120, 3);
+    cv::Canny(image, cannyResult, 50, 125, 3);
     cv::resize(cannyResult, cannyResult, cv::Size(image.cols*3, image.rows*3));
 
-    cv::imshow("Camera Feed", cannyResult);
+    //insert into window
+    if(prev_images.size() < prev_count){
+        prev_images.insert(prev_images.begin(), cannyResult/prev_count);
+    }else{
+        prev_images.pop_back();
+        prev_images.insert(prev_images.begin(), cannyResult/prev_count);
+    }
+
+    //find average from window
+    if(prev_images.size() < prev_count){
+      avg_result = cannyResult.clone();
+    }else{
+      avg_result = prev_images[0].clone();
+      for(int i = 1; i < prev_images.size(); i++){
+        avg_result = avg_result + prev_images[i];
+      }
+    }
+
+    cv::imshow("Camera Feed", avg_result);
 
     //stop camera if user hits 'esc'
     if(cv::waitKey(1000.0/FPS)==27){
