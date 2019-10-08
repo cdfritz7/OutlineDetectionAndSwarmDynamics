25e6a55641175edf87852b93083c80950b68123b
cdfritz7 <cdfritz7@utexas.edu>
2019-10-07 23:56:50 -0500

    added opencv kinect example

---
+++ exploration/kinect/OpenCvExample.cpp
@@ -0,0 +1,158 @@
+#include "libfreenect.hpp"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <pthread.h>
+#include <opencv.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+
+class myMutex {
+	public:
+		myMutex() {
+			pthread_mutex_init( &m_mutex, NULL );
+		}
+		void lock() {
+			pthread_mutex_lock( &m_mutex );
+		}
+		void unlock() {
+			pthread_mutex_unlock( &m_mutex );
+		}
+	private:
+		pthread_mutex_t m_mutex;
+};
+
+
+class MyFreenectDevice : public Freenect::FreenectDevice {
+	public:
+		MyFreenectDevice(freenect_context *_ctx, int _index)
+	 		: Freenect::FreenectDevice(_ctx, _index), m_buffer_depth(FREENECT_DEPTH_11BIT),
+			m_buffer_rgb(FREENECT_VIDEO_RGB), m_gamma(2048), m_new_rgb_frame(false),
+			m_new_depth_frame(false), depthMat(Size(640,480),CV_16UC1),
+			rgbMat(Size(640,480), CV_8UC3, Scalar(0)),
+			ownMat(Size(640,480), CV_8UC3, Scalar(0)) {
+
+			for( unsigned int i = 0 ; i < 2048 ; i++) {
+				float v = i/2048.0;
+				v = std::pow(v, 3)* 6;
+				m_gamma[i] = v*6*256;
+			}
+		}
+
+		// Do not call directly even in child
+		void VideoCallback(void* _rgb, uint32_t timestamp) {
+			m_rgb_mutex.lock();
+			uint8_t* rgb = static_cast<uint8_t*>(_rgb);
+			rgbMat.data = rgb;
+			m_new_rgb_frame = true;
+			m_rgb_mutex.unlock();
+		};
+
+		// Do not call directly even in child
+		void DepthCallback(void* _depth, uint32_t timestamp) {
+			m_depth_mutex.lock();
+			uint16_t* depth = static_cast<uint16_t*>(_depth);
+			depthMat.data = (uchar*) depth;
+			cout<<*depth<<std::endl;
+			m_new_depth_frame = true;
+			m_depth_mutex.unlock();
+		}
+
+		bool getVideo(Mat& output) {
+			m_rgb_mutex.lock();
+			if(m_new_rgb_frame) {
+				cv::cvtColor(rgbMat, output, cv::COLOR_RGB2BGR);
+				m_new_rgb_frame = false;
+				m_rgb_mutex.unlock();
+				return true;
+			} else {
+				m_rgb_mutex.unlock();
+				return false;
+			}
+		}
+
+		bool getDepth(Mat& output) {
+				m_depth_mutex.lock();
+				if(m_new_depth_frame) {
+					depthMat.copyTo(output);
+					m_new_depth_frame = false;
+					m_depth_mutex.unlock();
+					return true;
+				} else {
+					m_depth_mutex.unlock();
+					return false;
+				}
+		}
+
+	private:
+		std::vector<uint8_t> m_buffer_depth;
+		std::vector<uint8_t> m_buffer_rgb;
+		std::vector<uint16_t> m_gamma;
+		Mat depthMat;
+		Mat rgbMat;
+		Mat ownMat;
+		myMutex m_rgb_mutex;
+		myMutex m_depth_mutex;
+		bool m_new_rgb_frame;
+		bool m_new_depth_frame;
+};
+
+
+int main(int argc, char **argv) {
+	bool die(false);
+	string filename("snapshot");
+	string suffix(".png");
+	int i_snap(0);
+
+	int width = 1280;
+	int height = 960;
+
+	Mat depthMat(Size(width,height),CV_16UC1);
+	Mat depthf (Size(width,height),CV_8UC3);
+	Mat rgbMat(Size(width,height),CV_8UC3, Scalar(0));
+	Mat ownMat(Size(width,height),CV_8UC3, Scalar(0));
+
+	// The next two lines must be changed as Freenect::Freenect
+	// isn't a template but the method createDevice:
+	// Freenect::Freenect<MyFreenectDevice> freenect;
+	// MyFreenectDevice& device = freenect.createDevice(0);
+	// by these two lines:
+
+	Freenect::Freenect freenect;
+	MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
+
+	namedWindow("rgb",cv::WINDOW_AUTOSIZE);
+	namedWindow("depth",cv::WINDOW_AUTOSIZE);
+	device.startVideo();
+	device.startDepth();
+
+	while (!die) {
+		device.getVideo(rgbMat);
+		device.getDepth(depthMat);
+		cv::imshow("rgb", rgbMat);
+		depthMat.convertTo(depthf, CV_8UC3, 255.0/2048.0);
+		cv::threshold(depthf, depthf, 125, 255, THRESH_BINARY);
+		cv::imshow("depth",depthf);
+		char k = cv::waitKey(5);
+
+		if( k == 27 ){
+			cv::destroyWindow("rgb");
+			cv::destroyWindow("depth");
+			break;
+		}
+
+		if( k == 8 ) {
+			std::ostringstream file;
+			file << filename << i_snap << suffix;
+			cv::imwrite(file.str(),rgbMat);
+			i_snap++;
+		}
+	}
+
+	device.stopVideo();
+	device.stopDepth();
+	return 0;
+}
