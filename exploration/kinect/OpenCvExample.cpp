1f5bc1938a53f100ad30a75c87ec1fa249886559
cdfritz7 <cdfritz7@utexas.edu>
2019-10-10 21:13:12 -0500

    added contour finding

--- exploration/kinect/OpenCvExample.cpp
+++ exploration/kinect/OpenCvExample.cpp
@@ -5,7 +5,6 @@
 #include <pthread.h>
 #include <opencv.hpp>
 
-
 using namespace cv;
 using namespace std;
 
@@ -56,7 +55,6 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 			m_depth_mutex.lock();
 			uint16_t* depth = static_cast<uint16_t*>(_depth);
 			depthMat.data = (uchar*) depth;
-			cout<<*depth<<std::endl;
 			m_new_depth_frame = true;
 			m_depth_mutex.unlock();
 		}
@@ -101,8 +99,8 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 };
 
 void filter(cv::Mat mat){
-	  for(int i = 0; i<mat.rows; i++){
-			for(int j = 0; j<mat.cols; j++){
+	  for(signed i = 0; i<mat.rows; i++){
+			for(signed j = 0; j<mat.cols; j++){
 				int mat_val = mat.data[mat.step[0]*i + mat.step[1]* j + 0];
 				if(mat_val == 255 or mat_val > 110){
 					mat.data[mat.step[0]*i + mat.step[1]* j] = 0;
@@ -111,19 +109,38 @@ void filter(cv::Mat mat){
 		}
 }
 
+vector<vector<Point>> drop_contours(vector<vector<Point>> contours, int prop){
+	vector<vector<Point>> ret_arr;
+
+	for(unsigned i = 0; i < contours.size(); i++){
+		vector<Point> contour;
+		for(unsigned j = 0; j < contours.at(i).size(); j++){
+			if(j%prop == 0){
+				contour.push_back(contours.at(i).at(j));
+			}
+		}
+		ret_arr.push_back(contour);
+	}
+
+	return ret_arr;
+}
+
 int main(int argc, char **argv) {
 	bool die(false);
 	string filename("snapshot");
 	string suffix(".png");
 	int i_snap(0);
 
-	int width = 1280;
-	int height = 960;
+	int width = 640;
+	int height = 480;
+
+	RNG rng(1235);
 
 	Mat depthMat(Size(width,height),CV_16UC1);
 	Mat depthf (Size(width,height),CV_8UC3);
+	Mat mask (Size(width,height), CV_8UC3);
 	Mat rgbMat(Size(width,height),CV_8UC3, Scalar(0));
-	Mat ownMat(Size(width,height),CV_8UC3, Scalar(0));
+	Mat cannyResult;
 
 	// The next two lines must be changed as Freenect::Freenect
 	// isn't a template but the method createDevice:
@@ -140,13 +157,38 @@ int main(int argc, char **argv) {
 	device.startDepth();
 
 	while (!die) {
+		Mat outMat(Size(width, height),CV_8UC3, Scalar(0));
+		vector<vector<Point>> contours;
+  	vector<Vec4i> hierarchy;
+
 		device.getVideo(rgbMat);
 		device.getDepth(depthMat);
-		cv::imshow("rgb", rgbMat);
+
 		depthMat.convertTo(depthf, CV_8UC3, 255.0/2048.0);
-		//cv::threshold(depthf, depthf, 125, 255, THRESH_BINARY);
-		filter(depthf);
-		cv::imshow("depth",depthf);
+		filter(depthf); //remove background
+		/*
+		cv::threshold(depthf, mask, 1, 255, THRESH_BINARY);
+		cv::cvtColor(mask, mask, cv::COLOR_GRAY2BGR);
+		*/
+		//find edges and contours
+		Canny(depthf, cannyResult, 10, 20, 3);
+		findContours(cannyResult, contours, hierarchy, cv::RETR_EXTERNAL,
+									cv::CHAIN_APPROX_TC89_L1, Point(0,0));
+		contours = drop_contours(contours, 4);
+
+		//draw contours
+		Mat drawing = Mat::zeros(cannyResult.size(), CV_8UC3 );
+	  for(unsigned i = 0; i < contours.size(); i++ )
+    {
+      Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    }
+
+		imshow("depth",depthf);
+		imshow("rgb", rgbMat);
+		imshow("canny", cannyResult);
+		imshow("contours", drawing);
+
 		char k = cv::waitKey(5);
 
 		if( k == 27 ){
