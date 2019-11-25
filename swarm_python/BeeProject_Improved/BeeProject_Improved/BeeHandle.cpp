dbdb044fa53fa90dc14e807c6ae97c3bd099196e
Bpham1 <brandon.pham@utexas.edu>
2019-11-25 12:22:03 -0600

    updated c++ swarm algo

--- swarm_python/BeeProject_Improved/BeeProject_Improved/BeeHandle.cpp
+++ swarm_python/BeeProject_Improved/BeeProject_Improved/BeeHandle.cpp
@@ -4,24 +4,37 @@
 
 using namespace std;
 
-int main() {
-
+void fps_test(int num_bees=400, int num_flowers=200, int frames=100) {
 	BeeHandle bee_handle;
 
-	for (int i = 0; i < 200; i++) {
+	for (int i = 0; i < num_bees; i++) {
 		bee_handle.add_bee();
 	}
-	for (int i = 0; i < 200; i++) {
-		bee_handle.add_flower(i + 50, 100);
+
+	for (int i = 0; i < num_flowers; i++) {
+		bee_handle.add_flower();
 	}
+
 	clock_t time_start = clock();
-	int i = 0;
-	while (i < 100) {
+
+	int frame = 0;
+	double total_fps = 0.0;
+	while (frame < frames) {
 		bee_handle.update_movement();
+		frame++;
 		double elapsed_secs = double(clock() - time_start) / CLOCKS_PER_SEC;
 		double fps = (double)1 / elapsed_secs;
-		cout << fps << endl;
 		time_start = clock();
-		i++;
+		total_fps += fps;
 	}
+	cout << "Average FPS: " << total_fps / frame << endl;
+}
+
+int main() {
+	cout << "Bees = 100; Flowers = 200" << endl;
+	fps_test(100, 200);
+	cout << "Bees = 200; Flowers = 200" << endl;
+	fps_test(200, 200);
+	cout << "Bees = 300; Flowers = 200" << endl;
+	fps_test(300, 200);
 }
--- swarm_python/BeeProject_Improved/BeeProject_Improved/BeeHandle.h
+++ swarm_python/BeeProject_Improved/BeeProject_Improved/BeeHandle.h
@@ -16,7 +16,8 @@ const int RandomNessFactor = M_PI/8;
 const int MatrixMaxMag = sqrt(pow(MatrixLength, 2) + pow(MatrixHeight, 2));
 
 
-float RandomFloat(float a, float b) {
+float RandomFloat(float a, float b) 
+{
 	float random = ((float)rand()) / (float)RAND_MAX;
 	float diff = b - a;
 	float r = random * diff;
@@ -35,7 +36,8 @@ private:
 	int num_flowers;
 	int num_bees;
 public:
-	BeeHandle() {
+	BeeHandle() 
+	{
 		bee_array_x = new int[MatrixHeight * MatrixLength];
 		bee_array_y = new int[MatrixHeight * MatrixLength];
 		flower_array_x = new int[MatrixHeight * MatrixLength];
@@ -45,7 +47,8 @@ public:
 		num_flowers = 0;
 		num_bees = 0;
 	};
-	~BeeHandle() {
+	~BeeHandle() 
+	{
 		delete[] bee_array_x;
 		delete[] bee_array_y;
 		delete[] flower_array_x;
@@ -53,13 +56,17 @@ public:
 		delete[] bee_flower;
 		delete[] flower_bee;
 	};
-	void add_bee(int x = -1, int y = -1) {
-		if (num_bees < MatrixHeight * MatrixLength) {
-			if (x < 0) {
+	void add_bee(int x = -1, int y = -1) 
+	{
+		if (num_bees < MatrixHeight * MatrixLength) 
+		{
+			if (x < 0) 
+			{
 				x = rand() % MatrixLength;
 			}
-			if (y < 0) {
-				x = rand() % MatrixHeight;
+			if (y < 0) 
+			{
+				y = rand() % MatrixHeight;
 			}
 			bee_array_x[num_bees] = x;
 			bee_array_y[num_bees] = y;
@@ -70,13 +77,17 @@ public:
 			cout << "Max number of bees reached" << endl;
 		}
 	}
-	void add_flower(int x = -1, int y = -1) {
-		if (num_flowers < MatrixHeight * MatrixLength) {
-			if (x < 0) {
+	void add_flower(int x = -1, int y = -1) 
+	{
+		if (num_flowers < MatrixHeight * MatrixLength) 
+		{
+			if (x < 0) 
+			{
 				x = rand() % MatrixLength;
 			}
-			if (y < 0) {
-				x = rand() % MatrixHeight;
+			if (y < 0) 
+			{
+				y = rand() % MatrixHeight;
 			}
 			flower_array_x[num_flowers] = x;
 			flower_array_y[num_flowers] = y;
@@ -87,85 +98,157 @@ public:
 			cout << "Max number of flowers reached" << endl;
 		}
 	}
-	void clear_flowers() {
+	void clear_flowers() 
+	{
 		num_flowers = 0;
 	}
-	void update_movement() {
-		for (int i = 0; i < num_bees; i++) {
+	void update_movement() 
+	{
+		for (int i = 0; i < num_bees; i++) 
+		{
 			int bee_x = bee_array_x[i];
 			int bee_y = bee_array_y[i];
-			int total_x = 0;
-			int total_y = 0;
 
-			for (int j = 0; j < num_bees; j++) {
-				if (j != i && bee_flower[j] == -1) {
-					int bee2_x = bee_array_x[j];
-					int bee2_y = bee_array_y[j];
-					int dist_x = bee2_x - bee_x;
-					int dist_y = bee2_y - bee_y;
+			if (bee_flower[i] == -1) 
+			{
+				int total_x = 0;
+				int total_y = 0;
 
-					float dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
-					dist = pow((MatrixMaxMag - dist), 2);
+				for (int j = 0; j < num_bees; j++) 
+				{
+					if (j != i && bee_flower[j] == -1) 
+					{
+						int bee2_x = bee_array_x[j];
+						int bee2_y = bee_array_y[j];
+						int dist_x = bee2_x - bee_x;
+						int dist_y = bee2_y - bee_y;
 
-					total_x -= dist_x * dist * (BeeStrength / FlowerStrength);
-					total_y -= dist_y * dist * (BeeStrength / FlowerStrength);
-				}
-			}
-			int flower_in_range = -1;
-			int flower_dist = -1;
-			for (int j = 0; j < num_flowers; j++) {
-				if (flower_bee[j] != i) {
-					int flower_x = flower_array_x[j];
-					int flower_y = flower_array_y[j];
-					int dist_x = flower_x - bee_x;
-					int dist_y = flower_y - bee_y;
+						float dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
+						dist = pow((MatrixMaxMag - dist), 2);
 
-					float dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
-
-					if (dist < MoveMax && (flower_in_range < 0 || dist < flower_dist)) {
-						flower_in_range = j;
-						flower_dist = j;
+						total_x -= dist_x * dist * (BeeStrength / FlowerStrength);
+						total_y -= dist_y * dist * (BeeStrength / FlowerStrength);
 					}
-
-					dist = pow((MatrixMaxMag - dist), 2);
-
-					total_x += dist_x * dist * (BeeStrength / FlowerStrength);
-					total_y += dist_y * dist * (BeeStrength / FlowerStrength);
-				} else {
-					bee_flower[i] = -1;
-					flower_bee[j] = -1;
 				}
-			}
-			if (flower_in_range < 0) {
-				float rads = atan2(total_y, total_x) + RandomFloat(-1 * RandomNessFactor, RandomNessFactor);
-				bee_array_x[i] = bee_x + int(cos(rads) * RandomFloat(0, (float)MoveMax));
-				bee_array_y[i] = bee_y + int(sin(rads) * RandomFloat(0, (float)MoveMax));
+
+				int flower_in_range = -1;
+				int flower_dist = -1;
+				int num_empty_flowers = 0;
+
+				for (int j = 0; j < num_flowers; j++) 
+				{
+					if (flower_bee[j] == -1) 
+					{
+						num_empty_flowers++;
+						int flower_x = flower_array_x[j];
+						int flower_y = flower_array_y[j];
+						int dist_x = flower_x - bee_x;
+						int dist_y = flower_y - bee_y;
+
+						float dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
+
+						if (dist < MoveMax && (flower_in_range < 0 || dist < flower_dist)) 
+						{
+							flower_in_range = j;
+							flower_dist = j;
+						}
+
+						dist = pow((MatrixMaxMag - dist), 2);
+
+						total_x += dist_x * dist * (BeeStrength / FlowerStrength);
+						total_y += dist_y * dist * (BeeStrength / FlowerStrength);
+					}
+				}
+
+				if (num_empty_flowers == 0) 
+				{
+					total_x = RandomFloat(-1.0, 1.0);
+					total_y = RandomFloat(-1.0, 1.0); 
+				}
+
+				if (flower_in_range < 0) 
+				{
+					float rads = atan2(total_y, total_x) + RandomFloat(-1 * RandomNessFactor, RandomNessFactor);
+					
+					if (!(total_x == 0 && total_y == 0)) 
+					{
+						int new_x = bee_x + int(cos(rads) * RandomFloat(0, (float)MoveMax));
+						int new_y = bee_y + int(sin(rads) * RandomFloat(0, (float)MoveMax));
+
+						if (new_x < 0 or new_x >= MatrixLength)
+						{
+							bee_array_x[i] = bee_x - int(cos(rads) * RandomFloat(0, (float)MoveMax));
+						}
+						else
+						{
+							bee_array_x[i] = new_x;
+						}
+						
+						if (new_y < 0 or new_y >= MatrixHeight) 
+						{
+							bee_array_y[i] = bee_y + int(sin(rads) * RandomFloat(0, (float)MoveMax));
+						}
+						else {
+							bee_array_y[i] = new_y;
+						}
+					}
+				}
+				else {
+					bee_flower[i] = flower_in_range;
+					flower_bee[flower_in_range] = i;
+					bee_array_x[i] = flower_array_x[flower_in_range];
+					bee_array_y[i] = flower_array_y[flower_in_range];
+				}
 			}
 			else {
-				if (flower_bee[flower_in_range] != -1) {
-					bee_flower[flower_bee[flower_in_range]] = -1;
-					flower_bee[flower_in_range] = -1;
+				int flower_in_range = -1;
+				int flower_dist = -1;
+
+				for (int j = 0; j < num_flowers; j++) 
+				{
+					if (flower_bee[j] == -1) 
+					{
+						int flower_x = flower_array_x[j];
+						int flower_y = flower_array_y[j];
+						int dist_x = flower_x - bee_x;
+						int dist_y = flower_y - bee_y;
+
+						float dist = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
+
+						if (dist < MoveMax && (flower_in_range < 0 || dist < flower_dist)) 
+						{
+							flower_in_range = j;
+							flower_dist = j;
+						}
+					}
+
+					if (flower_in_range != -1) {
+						bee_flower[i] = flower_in_range;
+						flower_bee[flower_in_range] = i;
+						bee_array_x[i] = flower_array_x[flower_in_range];
+						bee_array_y[i] = flower_array_y[flower_in_range];
+					}
 				}
-				bee_flower[i] = flower_in_range;
-				flower_bee[flower_in_range] = i;
-				bee_array_x[i] = flower_array_x[flower_in_range];
-				bee_array_y[i] = flower_array_y[flower_in_range];
 			}
 		}
 	}
-	int getNumBees() {
+	int getNumBees() 
+	{
 		return num_bees;
 	}
-	int getNumFlowers() {
+	int getNumFlowers() 
+	{
 		return num_flowers;
 	}
-	int* getBeeCoords(int idx) {
+	int* getBeeCoords(int idx) 
+	{
 		int temp[2];
 		temp[0] = bee_array_x[idx];
 		temp[1] = bee_array_y[idx];
 		return temp;
 	}
-	int* getFlowerCoords(int idx) {
+	int* getFlowerCoords(int idx) 
+	{
 		int temp[2];
 		temp[0] = flower_array_x[idx];
 		temp[1] = flower_array_y[idx];
