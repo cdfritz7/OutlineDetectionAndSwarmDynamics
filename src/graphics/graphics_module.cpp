bd2b0a966f6154a4688be7ceb281efee7b674b84
cdfritz7 <cdfritz7@utexas.edu>
2020-04-08 14:23:02 -0500

    finished adding text to the graphics module

--- src/graphics/graphics_module.cpp
+++ src/graphics/graphics_module.cpp
@@ -21,6 +21,7 @@ using namespace std;
 #include "common/shader.hpp"
 #include "common/texture.hpp"
 #include "common/controls.hpp"
+#include "common/text2D.hpp"
 #include "graphics_module.hpp"
 
 // CPU representation of a particle
@@ -163,6 +164,22 @@ GraphicsModule::GraphicsModule(int num_particles, int maxX, int maxY,
 	if(mdir.substr(mdir.length()-1, mdir.length()-1) != "/")
 		mdir = mdir + "/";
 
+  // Ensure we can capture the escape key being pressed below
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  // black background
+  glClearColor(0.0f, 0.0f, 0.2f, 0.0f);
+
+  // Enable depth test
+  glEnable(GL_DEPTH_TEST);
+
+  // Accept fragment if it closer to the camera than the former one
+  glDepthFunc(GL_LESS);
+
+	//initialize our text variables
+	initText2D( "Holstein.DDS" , mdir.c_str(), max_x, max_y);
+
+	//load shaders for our particles
   programID = LoadShaders((mdir+"shaders/Particle.vertexshader").c_str(),
 	                        (mdir+"shaders/Particle.fragmentshader").c_str());
 	if(programID == 0){
@@ -171,20 +188,6 @@ GraphicsModule::GraphicsModule(int num_particles, int maxX, int maxY,
 		is_init = false;
 		return;
 	}
-
-  // Ensure we can capture the escape key being pressed below
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-  // black background
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-  // Enable depth test
-  glEnable(GL_DEPTH_TEST);
-  // Accept fragment if it closer to the camera than the former one
-  glDepthFunc(GL_LESS);
-
-  glGenVertexArrays(1, &VertexArrayID);
-  glBindVertexArray(VertexArrayID);
 
   //load our texture
   Texture = loadPNG(texture_fp);
@@ -247,6 +250,7 @@ GraphicsModule::GraphicsModule(int num_particles, int maxX, int maxY,
 	//initialize our QR code variables
 	init_qr(mdir);
 
+	//initialize our
 	is_init = true;
 }
 
@@ -274,6 +278,225 @@ int GraphicsModule::update_particles(vector<int> x, vector<int> y, vector<int> s
     ParticlesContainer[i].direction = direction.at(i);
   }
   return 0;
+}
+
+/*
+update the window created by the instantiated GraphicsModule
+based on the array of Particles maintained by the GraphicsModule instance
+*/
+void GraphicsModule::update_display(){
+
+	if(!is_init)
+		return;
+
+  // Clear the screen
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Camera matrix
+  ViewMatrix = glm::lookAt(
+                camera_position,
+                glm::vec3(0,0,0), // and looks at the origin
+                glm::vec3(0,-1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+               );
+	// Projection matrix : 45� Field of View, screen_ratio, display range (z coords): 0.1 unit <-> 100 units
+  glm::mat4 ProjectionMatrix = glm::perspective(glm::radians(45.0f), screen_ratio, .1f, 100.0f);
+
+  // We will need the camera's position in order to sort the particles
+  // w.r.t the camera's distance.
+  // There should be a getCameraPosition() function in common/controls.cpp,
+  // but this works too.
+  glm::vec3 CameraPosition(glm::inverse(ViewMatrix)[3]);
+  ViewProjectionMatrix = ProjectionMatrix * ViewMatrix;
+
+
+	//code to render the QR code to the screen, if it has been enabled
+	render_qr();
+
+
+  // Simulate all particles
+  for(int i=0; i<MaxParticles; i++){
+
+    Particle& p = ParticlesContainer[i]; // shortcut
+
+    p.cameradistance = glm::length2( p.pos - CameraPosition );
+
+    // Fill the GPU buffer of positions
+    g_particule_position_size_data[4*i+0] = p.pos.x;
+    g_particule_position_size_data[4*i+1] = p.pos.y;
+    g_particule_position_size_data[4*i+2] = p.pos.z;
+
+    g_particule_position_size_data[4*i+3] = p.size;
+
+    //fill the GPU buffer with stage information
+    g_particule_stage_data[2*i+0] = p.stage;
+    g_particule_stage_data[2*i+1] = p.direction;
+
+  }
+
+  SortParticles();
+
+
+  // Update the buffers that OpenGL uses for rendering.
+  // There are much more sophisticated means to stream data from the CPU to the GPU,
+  // but this is outside the scope of this tutorial.
+  // http://www.opengl.org/wiki/Buffer_Object_Streaming
+  glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+  glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+  glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLfloat) * 4, g_particule_position_size_data);
+
+  glBindBuffer(GL_ARRAY_BUFFER, particles_stage_buffer);
+  glBufferData(GL_ARRAY_BUFFER, MaxParticles * 2 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+  glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLfloat) * 2, g_particule_stage_data);
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // Use our shader
+  glUseProgram(programID);
+
+  // Bind our texture in Texture Unit 0
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, Texture);
+  // Set our "myTextureSampler" sampler to use Texture Unit 0
+  glUniform1i(TextureID, 0);
+
+  // Same as the billboards tutorial
+  glUniform3f(CameraRight_worldspace_ID, ViewMatrix[0][0], ViewMatrix[1][0], ViewMatrix[2][0]);
+  glUniform3f(CameraUp_worldspace_ID   , ViewMatrix[0][1], ViewMatrix[1][1], ViewMatrix[2][1]);
+
+  glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE, &ViewProjectionMatrix[0][0]);
+
+  // 1rst attribute buffer : vertices
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
+  glVertexAttribPointer(
+    0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+    3,                  // size
+    GL_FLOAT,           // type
+    GL_FALSE,           // normalized?
+    0,                  // stride
+    (void*)0            // array buffer offset
+  );
+
+  // 2nd attribute buffer : positions of particles' centers
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+  glVertexAttribPointer(
+    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+    4,                                // size : x + y + z + size => 4
+    GL_FLOAT,                         // type
+    GL_FALSE,                         // normalized?
+    0,                                // stride
+    (void*)0                          // array buffer offset
+  );
+
+  //3rd attribute buffer : information about particle stage
+  glEnableVertexAttribArray(2);
+  glBindBuffer(GL_ARRAY_BUFFER, particles_stage_buffer);
+  glVertexAttribPointer(
+    2,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+    2,                                // size : x + y + z + size => 4
+    GL_FLOAT,                         // type
+    GL_FALSE,                         // normalized?
+    0,                                // stride
+    (void*)0                          // array buffer offset
+  );
+
+
+  // These functions are specific to glDrawArrays*Instanced*.
+  // The first parameter is the attribute buffer we're talking about.
+  // The second parameter is the "rate at which generic vertex attributes advance when rendering multiple instances"
+  // http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
+  glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
+  glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
+  glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
+
+
+  // Draw the particules !
+  // This draws many times a small triangle_strip (which looks like a quad).
+  // This is equivalent to :
+  // for(i in MaxParticles) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4),
+  // but faster.
+  glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, MaxParticles);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(2);
+
+	//text display
+	glVertexAttribDivisor(1, 0); //reset the stride at which vertices are read in
+	char text[256];
+	sprintf(text,"%.2f sec", glfwGetTime() );
+	for(unsigned i = 0; i < texts.size(); i++){
+		printText2D(texts[i].text.c_str(), texts[i].x, texts[i].y, texts[i].size);
+	}
+
+  // Swap buffers
+  glfwSwapBuffers(window);
+
+	//video recording
+	if((glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) && !record) {
+		record = true;
+		frame_count = 0;
+
+		cout<<cmd;
+		ffmpeg = popen(cmd.c_str(), "w");
+		if(ffmpeg == NULL) {
+			fprintf(stderr, "Could not open video file");
+			return;
+		}
+
+	} else if (record == true) {//only save even frames?
+		frame_count++;
+
+		if(frame_count%2 == 0){
+			void *pixels = malloc((int)((max_x*200)*(max_y*300)*3));
+			glReadPixels(0, 0, (int)(max_x*200), (int)(max_y*300), GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+			if (ffmpeg)
+	    	fwrite(pixels, (int)(max_x*200*max_y*300*3), 1, ffmpeg);
+			free(pixels);
+		}
+	}
+
+	if(frame_count >= frame_total) {
+		frame_count = 0;
+		record = false;
+		pclose(ffmpeg);
+	}
+
+  glfwPollEvents();
+}
+
+/*
+	adds a text struct to the class member vector<Text> texts
+	this will cause the text to be rendered at the next call to update_display()
+*/
+void GraphicsModule::add_text(string text, int x, int y, float size){
+	Text new_text;
+	new_text.text = text;
+	new_text.x = to_opengl_world_x(x);
+	new_text.y = to_opengl_world_y(y);
+	new_text.size = size;
+
+	texts.push_back(new_text);
+}
+
+/*
+	removes a text struct from our vector of texts, return false if the text is
+	not found. Will cause the text to be removed at the next call to update_display()
+*/
+bool GraphicsModule::remove_text(string text){
+	bool was_found = false;
+
+	for(unsigned i = 0; i < texts.size(); i++){
+		if(texts[i].text.compare(text) == 0){
+			texts.erase(texts.begin() + i);
+			was_found = true;
+		}
+	}
+
+	return was_found;
 }
 
 /*
@@ -360,182 +583,6 @@ void GraphicsModule::update_qr(bool enabled, const char* qrcode_fp, int x, int y
 	qr_enabled = enabled;
 }
 
-
-/*
-update the window created by the instantiated GraphicsModule
-based on the array of Particles maintained by the GraphicsModule instance
-*/
-void GraphicsModule::update_display(){
-
-	if(!is_init)
-		return;
-
-  // Clear the screen
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Camera matrix
-  ViewMatrix = glm::lookAt(
-                camera_position,
-                glm::vec3(0,0,0), // and looks at the origin
-                glm::vec3(0,-1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-               );
-	// Projection matrix : 45� Field of View, screen_ratio, display range (z coords): 0.1 unit <-> 100 units
-  glm::mat4 ProjectionMatrix = glm::perspective(glm::radians(45.0f), screen_ratio, .1f, 100.0f);
-
-  // We will need the camera's position in order to sort the particles
-  // w.r.t the camera's distance.
-  // There should be a getCameraPosition() function in common/controls.cpp,
-  // but this works too.
-  glm::vec3 CameraPosition(glm::inverse(ViewMatrix)[3]);
-  ViewProjectionMatrix = ProjectionMatrix * ViewMatrix;
-
-	//code to render the QR code to the screen, if it has been enabled
-	render_qr();
-
-  // Simulate all particles
-  for(int i=0; i<MaxParticles; i++){
-
-    Particle& p = ParticlesContainer[i]; // shortcut
-
-    p.cameradistance = glm::length2( p.pos - CameraPosition );
-
-    // Fill the GPU buffer of positions
-    g_particule_position_size_data[4*i+0] = p.pos.x;
-    g_particule_position_size_data[4*i+1] = p.pos.y;
-    g_particule_position_size_data[4*i+2] = p.pos.z;
-
-    g_particule_position_size_data[4*i+3] = p.size;
-
-    //fill the GPU buffer with stage information
-    g_particule_stage_data[2*i+0] = p.stage;
-    g_particule_stage_data[2*i+1] = p.direction;
-
-  }
-
-  SortParticles();
-
-  // Update the buffers that OpenGL uses for rendering.
-  // There are much more sophisticated means to stream data from the CPU to the GPU,
-  // but this is outside the scope of this tutorial.
-  // http://www.opengl.org/wiki/Buffer_Object_Streaming
-  glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
-  glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-  glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLfloat) * 4, g_particule_position_size_data);
-
-  glBindBuffer(GL_ARRAY_BUFFER, particles_stage_buffer);
-  glBufferData(GL_ARRAY_BUFFER, MaxParticles * 2 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
-  glBufferSubData(GL_ARRAY_BUFFER, 0, MaxParticles * sizeof(GLfloat) * 2, g_particule_stage_data);
-
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-  // Use our shader
-  glUseProgram(programID);
-
-  // Bind our texture in Texture Unit 0
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_2D, Texture);
-  // Set our "myTextureSampler" sampler to use Texture Unit 0
-  glUniform1i(TextureID, 0);
-
-  // Same as the billboards tutorial
-  glUniform3f(CameraRight_worldspace_ID, ViewMatrix[0][0], ViewMatrix[1][0], ViewMatrix[2][0]);
-  glUniform3f(CameraUp_worldspace_ID   , ViewMatrix[0][1], ViewMatrix[1][1], ViewMatrix[2][1]);
-
-  glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE, &ViewProjectionMatrix[0][0]);
-
-  // 1rst attribute buffer : vertices
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
-  glVertexAttribPointer(
-    0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-    3,                  // size
-    GL_FLOAT,           // type
-    GL_FALSE,           // normalized?
-    0,                  // stride
-    (void*)0            // array buffer offset
-  );
-
-  // 2nd attribute buffer : positions of particles' centers
-  glEnableVertexAttribArray(1);
-  glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
-  glVertexAttribPointer(
-    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-    4,                                // size : x + y + z + size => 4
-    GL_FLOAT,                         // type
-    GL_FALSE,                         // normalized?
-    0,                                // stride
-    (void*)0                          // array buffer offset
-  );
-
-  //3rd attribute buffer : information about particle stage
-  glEnableVertexAttribArray(2);
-  glBindBuffer(GL_ARRAY_BUFFER, particles_stage_buffer);
-  glVertexAttribPointer(
-    2,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-    2,                                // size : x + y + z + size => 4
-    GL_FLOAT,                         // type
-    GL_FALSE,                         // normalized?
-    0,                                // stride
-    (void*)0                          // array buffer offset
-  );
-
-  // These functions are specific to glDrawArrays*Instanced*.
-  // The first parameter is the attribute buffer we're talking about.
-  // The second parameter is the "rate at which generic vertex attributes advance when rendering multiple instances"
-  // http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
-  glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
-  glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
-  glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
-
-  // Draw the particules !
-  // This draws many times a small triangle_strip (which looks like a quad).
-  // This is equivalent to :
-  // for(i in MaxParticles) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4),
-  // but faster.
-  glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, MaxParticles);
-
-  glDisableVertexAttribArray(0);
-  glDisableVertexAttribArray(1);
-  glDisableVertexAttribArray(2);
-
-  // Swap buffers
-  glfwSwapBuffers(window);
-
-	//video recording
-	if((glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) && !record) {
-		record = true;
-		frame_count = 0;
-
-		cout<<cmd;
-		ffmpeg = popen(cmd.c_str(), "w");
-		if(ffmpeg == NULL) {
-			fprintf(stderr, "Could not open video file");
-			return;
-		}
-
-	} else if (record == true) {//only save even frames?
-		frame_count++;
-
-		if(frame_count%2 == 0){
-			void *pixels = malloc((int)((max_x*200)*(max_y*300)*3));
-			glReadPixels(0, 0, (int)(max_x*200), (int)(max_y*300), GL_RGB, GL_UNSIGNED_BYTE, pixels);
-
-			if (ffmpeg)
-	    	fwrite(pixels, (int)(max_x*200*max_y*300*3), 1, ffmpeg);
-			free(pixels);
-		}
-	}
-
-	if(frame_count >= frame_total) {
-		frame_count = 0;
-		record = false;
-		pclose(ffmpeg);
-	}
-
-  glfwPollEvents();
-}
-
 /*
 free the resources used by the graphics module
 */
@@ -553,13 +600,17 @@ void GraphicsModule::cleanup(){
   glDeleteBuffers(1, &billboard_vertex_buffer);
   glDeleteProgram(programID);
   glDeleteTextures(1, &Texture);
-  glDeleteVertexArrays(1, &VertexArrayID);
 
+	//clean up for QR code resources
 	if(qr_was_enabled){
 		glDeleteBuffers(1, &qr_vertex_buffer);
 		glDeleteProgram(QRProgramID);
 		glDeleteTextures(1, &QRTexture);
 	}
+
+	//clean up for text resources
+	texts.clear();
+	cleanupText2D();
 
   // Close OpenGL window and terminate GLFW
   glfwTerminate();
